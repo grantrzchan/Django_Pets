8f1f82f05b96c2c9b0532a928a2c38ce3150ef43
Grant Chan <grant.r.chan@gmail.com>
2020-03-28 11:04:17 -0500

    added models and migrations

--- adopt_a_pet/adoptions/models.py
+++ adopt_a_pet/adoptions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    #class variables
+    SEX_CHOICES = [('M', 'Male'),('F', 'Female')]
+
+
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50, blank=True) #some animals might not be a pure breed. Allow blank=true
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True) #pets rescued can be of an unknown age, use null to distinguish from age 0
+    vaccinations = models.ManyToManyField('Vaccine', blank=True) #many pets can have the same vaccine, each pet can have many vaccines
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
+
+    
